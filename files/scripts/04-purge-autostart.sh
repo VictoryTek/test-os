e3308f12085ddb4e8e9b0fc58768d27dd5ea9139
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2025-10-17 20:49:05 -0500

    Update 04-purge-autostart.sh

--- files/scripts/04-purge-autostart.sh
+++ files/scripts/04-purge-autostart.sh
@@ -5,22 +5,31 @@
 
 set -oue pipefail
 
-TARGET_DIR="/etc/xdg/autostart"
+# List of directories to check for autostart entries
+TARGET_DIRS=(
+  "/etc/xdg/autostart"
+  "/usr/etc/xdg/autostart"
+  "/etc/skel/.config/autostart"
+  "/usr/etc/skel/.config/autostart"
+)
+
 # List of entries (files or directories) to remove
 ITEMS=(
   "steam.desktop"
 )
 
-echo "::group:: Remove Waydroid desktop entries"
+echo "::group:: Remove autostart desktop entries"
 
-for item in "${ITEMS[@]}"; do
-  path="${TARGET_DIR}/${item}"
-  if [ -e "$path" ]; then
-    echo "Removing $path"
-    rm -rf "$path"
-  else
-    echo "Not present: $path (skipping)"
-  fi
+for target_dir in "${TARGET_DIRS[@]}"; do
+  for item in "${ITEMS[@]}"; do
+    path="${target_dir}/${item}"
+    if [ -e "$path" ]; then
+      echo "Removing $path"
+      rm -rf "$path"
+    else
+      echo "Not present: $path (skipping)"
+    fi
+  done
 done
 
 echo "::endgroup::"
